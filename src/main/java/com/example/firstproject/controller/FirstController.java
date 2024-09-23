50f60fbd2bc6beef2697d1feebd05a78d44b0d62
BaekEuiHun <euihunbaek@gmail.com>
2024-09-24 07:25:15 +0900

    Model을 이용한 변수 삽입

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,12 +1,14 @@
 package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
   @GetMapping("/hi")
-  public String niceToMeetYou() {
+  public String niceToMeetYou(Model model) {
+    model.addAttribute("username", "보리1");
     return "greetings";
   }
 }
