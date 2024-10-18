cf517ccedd8ce1a2adf06fdff77627f595f89c5e
BaekEuiHun <euihunbaek@gmail.com>
2024-10-18 13:36:55 +0900

    @NoArgsConstructor 추가 및 코드 정렬

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -5,21 +5,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class Article {
-    @Id
-    @GeneratedValue
-    private Long id;
+  @Id @GeneratedValue private Long id;
 
-    @Column
-    private String title;
+  @Column private String title;
 
-    @Column
-    private String content;
-
-
+  @Column private String content;
 }
