fa4db065868a2603545d1bf48ac39ea0f5fdeb77
BaekEuiHun <euihunbaek@gmail.com>
2024-10-23 19:21:56 +0900

    데이터 수정하기

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -3,30 +3,43 @@ package com.example.firstproject.api;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class ArticleApiController {
-    @Autowired
-    private ArticleRepository articleRepository;
+  @Autowired private ArticleRepository articleRepository;
 
-    @GetMapping("/api/articles")
-    public List<Article> index() {
-        return articleRepository.findAll();
+  @GetMapping("/api/articles")
+  public List<Article> index() {
+    return articleRepository.findAll();
+  }
+
+  @GetMapping("api/articles/{id}")
+  public Article show(@PathVariable Long id) {
+    return articleRepository.findById(id).orElse(null);
+  }
+
+  @PostMapping("api/articles")
+  public Article create(@RequestBody ArticleForm dto) {
+    Article article = dto.toEntity();
+    return articleRepository.save(article);
+  }
+
+  @PatchMapping("api/articles/{id}")
+  public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
+    Article article = dto.toEntity();
+    Article target = articleRepository.findById(id).orElse(null);
+    if (target != null || id != article.getId()) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
     }
-
-    @GetMapping("api/articles/{id}")
-    public Article show(@PathVariable Long id) {
-        return articleRepository.findById(id).orElse(null);
-    }
-
-    @PostMapping("api/articles")
-    public Article create(@RequestBody ArticleForm dto) {
-        Article article = dto.toEntity();
-        return articleRepository.save(article);
-    }
-
+    Article updated = articleRepository.save(article);
+    return ResponseEntity.status(HttpStatus.OK).body(updated);
+  }
 }
