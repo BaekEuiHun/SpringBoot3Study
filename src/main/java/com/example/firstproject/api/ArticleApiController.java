d2399c2e5bfb599bce9cd0043768c97fade006f7
BaekEuiHun <euihunbaek@gmail.com>
2024-10-23 19:29:54 +0900

    삭제하기 기능

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -43,4 +43,14 @@ public class ArticleApiController {
     Article updated = articleRepository.save(target);
     return ResponseEntity.status(HttpStatus.OK).body(updated);
   }
+
+  @DeleteMapping("api/articles/{id}")
+  public ResponseEntity<Article> delete(@PathVariable Long id) {
+    Article target = articleRepository.findById(id).orElse(null);
+    if (target == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+    }
+    articleRepository.delete(target);
+    return ResponseEntity.status(HttpStatus.OK).body(null);
+  }
 }
