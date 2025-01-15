18b8228da636abfc2f449540628e1b95929b14f9
EuiHun_Bori <euihunbaek@gmail.com>
2025-01-15 20:36:43 +0900

    Update Comment.java

--- src/main/java/com/example/firstproject/entity/Comment.java
+++ src/main/java/com/example/firstproject/entity/Comment.java
@@ -11,6 +11,7 @@ import lombok.ToString;
 @ToString // 모든 필드를 출력할 수 있는 toString 메서드 자동 생성
 @AllArgsConstructor // 모든 필드를 매개변수로 갖는 생성자 자동 생성
 @NoArgsConstructor // 매개변수가 아예 없는 기본 생성자 자동 생성
+  
 public class Comment {
   @Id // 대표키 지정
   @GeneratedValue(strategy= GenerationType.IDENTITY) // DB가 자동으로 1씩 증가
